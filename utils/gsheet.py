0057aa707eb15ae7945801605c6f2f58a3182259
Mahesh ramavat <Mramavat52@gmail.com>
2025-08-05 19:31:43 +0530

    Create utils/gsheet.py

---
+++ utils/gsheet.py
@@ -0,0 +1,8 @@
+Google Sheet read/write functions
+def read_google_sheet():
+    # TODO: Add real implementation
+    return []
+
+def update_signal(data):
+    # TODO: Push signal to Google Sheet
+    pass
